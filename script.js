5aafd4d16669ec34251820dbcb4cecc40d5b9982
JinYana <ngjinyang@gmail.com>
2021-02-02 23:36:54 +0800

    Added js code for index.html

--- script.js
+++ script.js
@@ -37,3 +37,40 @@ $(document).ready(function(){
           });
     })
 })
+
+// Code for Iteminventory(to display items)
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://shopinventory-7a51.restdb.io/rest/items",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "6017c9516adfba69db8b6c31",
+      "cache-control": "no-cache"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    for (i = 0; i < response.length; i++){
+       let item = document.createElement("div");
+       item.setAttribute("class", "item")
+       let title = document.createElement("p")
+       let price = document.createElement("p")
+       let img = document.createElement("img")
+       img.src = "https://shopinventory-7a51.restdb.io/media/" + response[i].ItemImage
+       img.setAttribute("width", "100px")
+       img.setAttribute("height", "100px")
+       let node = document.createTextNode(response[i].ItemName);
+       let number = document.createTextNode(response[i].ItemPrice)
+       item.appendChild(img)
+       title.appendChild(node)
+       price.appendChild(number)
+       item.appendChild(title)
+       item.appendChild(price)
+       
+       document.getElementById("inventory").appendChild(item)
+       
+    }
+  });
